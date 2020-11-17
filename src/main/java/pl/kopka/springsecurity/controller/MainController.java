27414ce85956a4bc19410c355d51ceb6bcbcd5d6
FIRST_NAME LAST_NAME <j.kopka@pwpw.pl>
2020-11-17 12:29:22 +0100

    Zad 1 Napisz aplikacje, która posiada 4 endpointy zwracające: 1. „Cześć admin {imie}” 2. „Cześć user {imie}” 3. „Cześć nieznajomy” 4. „Papa” Administrator ma mieć dostęp do wszystkich endpointów. User tylko do 2, 3, 4. Natomiast niezalogowany do 3. Skonfiguruj aplikacje w taki sposób, aby po wylogowaniu się użytkownik aplikacji (bez względu na role) zawsze zostawał przekierowany do endpointu z napisem „Papa”. Kiedy Administrator lub User wejdą na endpoint 3 to dostaną komunikat „Cześć {imie}”

---
+++ src/main/java/pl/kopka/springsecurity/controller/MainController.java
@@ -0,0 +1,41 @@
+package pl.kopka.springsecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@Controller
+@RestController
+public class MainController {
+
+    @GetMapping
+    public String home(Principal principal) {
+        if(principal != null){
+            return "Cześć " + principal.getName();
+        }
+        return "Cześć nieznajomy";
+//        try {
+//            return "Cześć " + principal.getName();
+//        } catch (NullPointerException ex) {
+//            return "Cześć nieznajomy";
+//        }
+    }
+
+    @GetMapping("/admin")
+    public String admin(Principal principal) {
+        return "Cześć admin " + principal.getName();
+    }
+
+    @GetMapping("/user")
+    public String user(Principal principal) {
+        return "Cześć user " + principal.getName();
+    }
+
+    @GetMapping("/papa")
+    public String logout() {
+        return "Papa";
+    }
+
+}
---
+++ src/main/java/pl/kopka/springsecurity/security/WebSecurityConfig.java
@@ -0,0 +1,49 @@
+package pl.kopka.springsecurity.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import pl.kopka.springsecurity.model.MyUser;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        MyUser user = new MyUser("user", getPasswordEncoder().encode("user"),
+                Collections.singleton(new SimpleGrantedAuthority("ROLE_USER")));
+
+        MyUser admin = new MyUser("admin", getPasswordEncoder().encode("admin"),
+                Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"), new SimpleGrantedAuthority("ROLE_ADMIN")));
+
+        auth.inMemoryAuthentication().withUser(user);
+        auth.inMemoryAuthentication().withUser(admin);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/admin").hasAnyRole("ADMIN")
+                .antMatchers("/user").hasAnyRole("USER")
+                .and()
+                .formLogin().permitAll()
+                .and().logout().logoutSuccessUrl("/papa");
+    }
+
+
+}
