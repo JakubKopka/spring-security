381999527056d4b23ebfe8f629fbf45686026e47
FIRST_NAME LAST_NAME <j.kopka@pwpw.pl>
2021-01-28 07:41:59 +0100

    Changing the key storage to application.properties

--- src/main/java/pl/kopka/springsecurity/JwtFilter.java
+++ src/main/java/pl/kopka/springsecurity/JwtFilter.java
@@ -5,6 +5,7 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -28,6 +29,12 @@ import java.util.Base64;
 import java.util.Collections;
 
 public class JwtFilter extends OncePerRequestFilter {
+
+    @Value("${privateKey}")
+    String privateKey;
+
+    @Value("${publicKey}")
+    String publicKey;
 
     @Override
     protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
@@ -60,15 +67,6 @@ public class JwtFilter extends OncePerRequestFilter {
     }
 
     private PrivateKey getPrivateKey() throws NoSuchAlgorithmException, InvalidKeySpecException {
-        String privateKey = "MIIBVAIBADANBgkqhkiG9w0BAQEFAASCAT4wggE6AgEAAkEAtq1bp+nhzwWODCx9" +
-                "wV52TOPpMN3T6dGlhM8m/ytVmRKgPIQssvCfAN4nGLLqPNeRgbxEsj2buglD5rp1" +
-                "0zIydQIDAQABAkA7bsdlJ/ipa/s9BrSbVupSNcxGc8VgSy74uJTNbAHbXN/dKrc1" +
-                "fObj0bxgeG31siNeWZHfznkqgKGPFrbSF/mBAiEA+Okxndbq3g+5s/U/S1KceUwo" +
-                "4tqfR6JHIOAQTaW1sEUCIQC74UFDrE7Lw1ewzP78UrWL/jDnN3L/b6XAxOoan/IU" +
-                "cQIhAJwKrNUzj4foudVsUOKqhnewXA1jWukwfYDJls7mrJE1AiBdivuprALXZP0m" +
-                "JArYqn2FpBvJI0x4bahDYEeG1hf7oQIgO5vD82hyykCKSAO7lbbmLifsEc8nMVLw" +
-                "YCg5PbuJEII=";
-
         PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(Base64.getDecoder().decode(privateKey));
         KeyFactory kf = KeyFactory.getInstance("RSA");
         PrivateKey key = kf.generatePrivate(keySpec);
@@ -76,9 +74,6 @@ public class JwtFilter extends OncePerRequestFilter {
     }
 
     private PublicKey getPublicKey() throws NoSuchAlgorithmException, InvalidKeySpecException {
-        String publicKey =
-               "MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBALatW6fp4c8FjgwsfcFedkzj6TDd0+nR" +
-                       "pYTPJv8rVZkSoDyELLLwnwDeJxiy6jzXkYG8RLI9m7oJQ+a6ddMyMnUCAwEAAQ==";
         X509EncodedKeySpec keySpec = new X509EncodedKeySpec(Base64.getDecoder().decode(publicKey));
         KeyFactory kf = KeyFactory.getInstance("RSA");
         PublicKey key = kf.generatePublic(keySpec);
