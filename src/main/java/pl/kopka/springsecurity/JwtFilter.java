663946dd78d45b6df0ef64c7d416b894deed3d66
FIRST_NAME LAST_NAME <j.kopka@pwpw.pl>
2021-01-28 11:38:32 +0100

    Changing the key storage to public.key and private.key

--- src/main/java/pl/kopka/springsecurity/JwtFilter.java
+++ src/main/java/pl/kopka/springsecurity/JwtFilter.java
@@ -5,7 +5,7 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
-import org.springframework.beans.factory.annotation.Value;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -15,7 +15,11 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import java.io.*;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.security.KeyFactory;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
@@ -29,12 +33,6 @@ import java.util.Base64;
 import java.util.Collections;
 
 public class JwtFilter extends OncePerRequestFilter {
-
-    @Value("${privateKey}")
-    private String privateKey;
-
-    @Value("${publicKey}")
-    private String publicKey;
 
     @Override
     protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
@@ -51,7 +49,7 @@ public class JwtFilter extends OncePerRequestFilter {
         filterChain.doFilter(httpServletRequest, httpServletResponse);
     }
 
-    private UsernamePasswordAuthenticationToken getUsernamePasswordAuthenticationToken(String authorization) throws InvalidKeySpecException, NoSuchAlgorithmException {
+    private UsernamePasswordAuthenticationToken getUsernamePasswordAuthenticationToken(String authorization) throws InvalidKeySpecException, NoSuchAlgorithmException, IOException {
         PrivateKey privateKey = getPrivateKey();
         PublicKey publicKey = getPublicKey();
         JWTVerifier jwtVerifier = JWT.require(Algorithm.RSA256((RSAPublicKey) publicKey, (RSAPrivateKey) privateKey)).build();
@@ -66,14 +64,25 @@ public class JwtFilter extends OncePerRequestFilter {
         return new UsernamePasswordAuthenticationToken(name, null, Collections.singleton(simpleGrantedAuthority));
     }
 
-    private PrivateKey getPrivateKey() throws NoSuchAlgorithmException, InvalidKeySpecException {
+    private String getKey(String filename) throws IOException {
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource(filename).getFile());
+        if(!file.exists()){
+            throw new IOException(filename + " does NOT exists!");
+        }
+        return new String(Files.readAllBytes(file.toPath()));
+    }
+
+    private PrivateKey getPrivateKey() throws NoSuchAlgorithmException, InvalidKeySpecException, IOException {
+        String privateKey = getKey("private.key");
         PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(Base64.getDecoder().decode(privateKey));
         KeyFactory kf = KeyFactory.getInstance("RSA");
         PrivateKey key = kf.generatePrivate(keySpec);
         return key;
     }
 
-    private PublicKey getPublicKey() throws NoSuchAlgorithmException, InvalidKeySpecException {
+    private PublicKey getPublicKey() throws NoSuchAlgorithmException, InvalidKeySpecException, IOException {
+        String publicKey = getKey("public.key");
         X509EncodedKeySpec keySpec = new X509EncodedKeySpec(Base64.getDecoder().decode(publicKey));
         KeyFactory kf = KeyFactory.getInstance("RSA");
         PublicKey key = kf.generatePublic(keySpec);
