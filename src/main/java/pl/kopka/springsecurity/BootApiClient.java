4d23aa73c0814ee097e3560a33bef3ef525edefa
FIRST_NAME LAST_NAME <j.kopka@pwpw.pl>
2021-01-28 11:41:16 +0100

    Changing the key storage to private.key

--- src/main/java/pl/kopka/springsecurity/BootApiClient.java
+++ src/main/java/pl/kopka/springsecurity/BootApiClient.java
@@ -12,6 +12,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.security.KeyFactory;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
@@ -24,15 +27,12 @@ import java.util.stream.Stream;
 @Controller
 public class BootApiClient {
 
-    @Value("${privateKey}")
-    private String privateKey;
-
-    public BootApiClient() throws InvalidKeySpecException, NoSuchAlgorithmException {
+    public BootApiClient() throws InvalidKeySpecException, NoSuchAlgorithmException, IOException {
         addBooks();
         getBooks();
     }
 
-    private void addBooks() throws InvalidKeySpecException, NoSuchAlgorithmException {
+    private void addBooks() throws InvalidKeySpecException, NoSuchAlgorithmException, IOException {
         String jwt = generateJwt(true);
         MultiValueMap<String, String> headers = new HttpHeaders();
         headers.add("Authorization", "Bearer " + jwt);
@@ -46,7 +46,7 @@ public class BootApiClient {
                 Void.class);
     }
 
-    private void getBooks() throws InvalidKeySpecException, NoSuchAlgorithmException {
+    private void getBooks() throws InvalidKeySpecException, NoSuchAlgorithmException, IOException {
         String jwt = generateJwt(true);
         MultiValueMap<String, String> headers = new HttpHeaders();
         headers.add("Authorization", "Bearer " + jwt);
@@ -60,16 +60,27 @@ public class BootApiClient {
         Stream.of(exchange.getBody()).forEach(System.out::println);
     }
 
-    private String generateJwt(boolean isAdmin) throws InvalidKeySpecException, NoSuchAlgorithmException {
+    private String generateJwt(boolean isAdmin) throws InvalidKeySpecException, NoSuchAlgorithmException, IOException {
         Algorithm algorithm = Algorithm.RSA256(null, (RSAPrivateKey) getPrivateKey());
         return JWT.create().withClaim("admin", isAdmin).sign(algorithm);
     }
 
 
-    private PrivateKey getPrivateKey() throws NoSuchAlgorithmException, InvalidKeySpecException {
+    private PrivateKey getPrivateKey() throws NoSuchAlgorithmException, InvalidKeySpecException, IOException {
+        String privateKey = getKey("private.key");
         PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(Base64.getDecoder().decode(privateKey));
         KeyFactory kf = KeyFactory.getInstance("RSA");
 
         return kf.generatePrivate(keySpec);
     }
+
+    private String getKey(String filename) throws IOException {
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource(filename).getFile());
+        if(!file.exists()){
+            throw new IOException(filename + " does NOT exists!");
+        }
+        return new String(Files.readAllBytes(file.toPath()));
+    }
+
 }
